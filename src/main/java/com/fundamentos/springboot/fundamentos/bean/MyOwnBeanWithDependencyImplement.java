97fc98795431cb2a4b6542eba9bf1581ebee7fbe
dimekaimorts <kaimorts817@gmail.com>
2022-10-13 17:12:49 -0500

    new: Implementation to apply dependency inside other dependency

--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyOwnBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyOwnBeanWithDependencyImplement.java
@@ -1,2 +1,20 @@
-package com.fundamentos.springboot.fundamentos.bean;public class MyOwnBeanWithDependencyImplement {
+package com.fundamentos.springboot.fundamentos.bean;
+
+import java.util.List;
+
+public class MyOwnBeanWithDependencyImplement implements MyOwnBeanWithDependency {
+
+    private MyOwnOperation myOwnOperation;
+
+    public MyOwnBeanWithDependencyImplement(MyOwnOperation myOwnOperation) {
+        this.myOwnOperation = myOwnOperation;
+    }
+
+    @Override
+    public void displayElements() {
+        System.out.println("Llamamos a MyOwnBeanOperationImplement para generar la lista aleatoria");
+        List<Integer> rand_nums = this.myOwnOperation.generateRandomElements(500, 10);
+        rand_nums.forEach(System.out::println);
+        System.out.println("Hola desde mi implementación de un bean con dependencia usando Streams y Listas");
+    }
 }
