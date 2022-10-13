31e597a3dca0f2866125ec7687d52986b5b4919a
dimekaimorts <kaimorts817@gmail.com>
2022-10-13 15:55:44 -0500

    new: Inyeccion de dependencia propia e inyeccion de dependencias anidadas usando @Qualifier

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/MyConfigurationBean.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/MyConfigurationBean.java
@@ -1,2 +1,23 @@
-package com.fundamentos.springboot.fundamentos.configuration;public class MyConfigurationBean {
+package com.fundamentos.springboot.fundamentos.configuration;
+
+import com.fundamentos.springboot.fundamentos.bean.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfigurationBean {
+    @Bean
+    public MyBean getBeanOperation() {
+        return new MyBeanTwoImplement();
+    }
+
+    @Bean
+    public MyOperation getBeanMyOperation() {
+        return new MyOperationImplement();
+    }
+
+    @Bean
+    public MyBeanWithDependency getBeanWithDependency(MyOperation myOperation) {
+        return new MyBeanWithDependencyImplement(myOperation);
+    }
 }
