93b6b822e9a6db7e066e63268c6a96b41768ed0d
dimekaimorts <kaimorts817@gmail.com>
2022-10-13 17:12:01 -0500

    new(interface): Interface to display elements. It is used by other dependency

--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyOwnBeanWithDependency.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyOwnBeanWithDependency.java
@@ -1,2 +1,5 @@
-package com.fundamentos.springboot.fundamentos.bean;public interface MyOwnBeanWithDependency {
+package com.fundamentos.springboot.fundamentos.bean;
+
+public interface MyOwnBeanWithDependency {
+    void displayElements();
 }
